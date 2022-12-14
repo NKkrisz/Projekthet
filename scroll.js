9e270ba4923899d5d1da22580c9f75ec3d024c33
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2022-12-14 15:41:58 +0100

    c

--- scroll.js
+++ scroll.js
@@ -1,5 +1,11 @@
 const scroll = document.querySelector(".scroll")
 const root = document.querySelector(":root")
+
+const img1 = document.getElementById('front')
+const img2 = document.getElementById('mid')
+const img3 = document.getElementById('back')
+
+
 function setScrollWidth() {
   root.style.setProperty("--scrollWidth", `-${parseInt(scroll.offsetWidth/2)}px`)
   console.log(`${scroll.offsetWidth}px`)
@@ -8,3 +14,9 @@ function setScrollWidth() {
 window.onresize = setScrollWidth
 
 setScrollWidth()
+
+function changeImage(src) {
+  img3.src = img2.src
+  img2.src = img1.src
+  img1.src = src
+}
